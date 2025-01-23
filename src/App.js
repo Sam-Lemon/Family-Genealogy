e1ac451e1af77ae98ae5ecef78868a545a017622
Sam-Lemon <theatrewhisper@gmail.com>
2025-01-22 20:03:58 -0800

    added API test component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ApiTest from "./components/APITest";
 
 function HomePage() {
   return <h1>Home Page</h1>;
@@ -27,7 +28,7 @@ function App() {
       <nav>
         <Link to="/">Home</Link> | <Link to="/lemon">Lemon</Link> |{" "}
         <Link to="/hart">Hart</Link> | <Link to="/steckle">Steckle</Link> |{" "}
-        <Link to="/keesler">Keesler</Link>
+        <Link to="/keesler">Keesler</Link> | <Link to="/api-test">API Test</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -35,6 +36,7 @@ function App() {
         <Route path="/hart" element={<HartFamily />} />
         <Route path="/steckle" element={<SteckleFamily />} />
         <Route path="/keesler" element={<KeeslerFamily />} />
+        <Route path="/api-test" element={<ApiTest />} />
       </Routes>
     </Router>
   );
